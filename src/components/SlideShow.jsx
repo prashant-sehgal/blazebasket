6977884394f04af5547466f5e2853b479fec46bd
Prashant Sehgal <prashantsehgal.95790@gmail.com>
2024-03-16 23:08:54 +0530

    adding action button to carousel window

--- src/components/SlideShow.jsx
+++ src/components/SlideShow.jsx
@@ -1,31 +1,63 @@
 import React, { useEffect, useState } from 'react'
-import { NavLink } from 'react-router-dom'
 
 export default function SlideShow() {
   const [activeSlide, setActiveSlide] = useState(1)
-  useEffect(function () {
-    setTimeout(function () {
-      if (activeSlide === 4) setActiveSlide(1)
-      else setActiveSlide(activeSlide + 1)
-    }, 5000)
-  })
+
+  function goBackToPreviousSlide() {
+    if (activeSlide === 1) setActiveSlide(4)
+    else setActiveSlide(activeSlide - 1)
+  }
+  function goToNextSlide() {
+    if (activeSlide === 4) setActiveSlide(1)
+    else setActiveSlide(activeSlide + 1)
+  }
+  function goToSlideLink(event) {
+    if (event.target.classList.contains('action-buttons')) {
+      console.log(`Clicked on slde: ${activeSlide}`)
+    }
+  }
   return (
     <div className="slide-show">
-      {[1, 2, 3, 4].map((value) => (
-        <NavLink to="/cart">
-          <div
-            className="image"
-            style={{
-              opacity: `${activeSlide === value ? 1 : 0}`,
-            }}
-          >
-            <img
-              src={`/slide-show/image-${value}.jpg`}
-              alt={`image-${value}`}
-            />
-          </div>
-        </NavLink>
-      ))}
+      <div className="action-buttons" onClick={goToSlideLink}>
+        <button onClick={goBackToPreviousSlide}>
+          <span className="material-symbols-outlined">arrow_back_ios</span>{' '}
+        </button>
+        <button onClick={goToNextSlide}>
+          <span className="material-symbols-outlined">arrow_forward_ios</span>{' '}
+        </button>
+      </div>
+      <div
+        className="image"
+        style={{
+          opacity: `${activeSlide === 1 ? 1 : 0}`,
+        }}
+      >
+        <img src={`/slide-show/image-${1}.jpg`} alt={`image-${1}`} />
+      </div>
+      <div
+        className="image"
+        style={{
+          opacity: `${activeSlide === 2 ? 1 : 0}`,
+        }}
+      >
+        <img src={`/slide-show/image-${2}.jpg`} alt={`image-${2}`} />
+      </div>
+      <div
+        className="image"
+        style={{
+          opacity: `${activeSlide === 3 ? 1 : 0}`,
+        }}
+      >
+        <img src={`/slide-show/image-${3}.jpg`} alt={`image-${3}`} />
+      </div>
+      <div
+        className="image"
+        style={{
+          opacity: `${activeSlide === 4 ? 1 : 0}`,
+        }}
+      >
+        <img src={`/slide-show/image-${4}.jpg`} alt={`image-${4}`} />
+      </div>
 
       <button></button>
     </div>
